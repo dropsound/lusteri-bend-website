423621cf141402c77733b1ed963ddc5b6a0cedf7
dropsound <dropsoundproduction@gmail.com>
2023-10-04 20:54:42 +0200

    vite installation

---
+++ lusteri-band/assets/js/shared/head.js
@@ -0,0 +1,17 @@
+document.querySelector('head').innerHTML = `
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Lusteri | bend</title>
+<link rel='shortcut icon' type='image/x-icon' href='/images/logos/lusteri-icon.png' />
+
+<meta name="description" content="Website benda Lusteri iz Srbije. Lusteri su akustični trio iz Srbije. 
+    Bend je osnovan u Novembru 2009. godine.">
+
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@200;400;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+`
---
+++ lusteri-band/assets/js/shared/header.js
@@ -0,0 +1,19 @@
+import '../../../style.scss'
+
+document.querySelector('#header').innerHTML = `
+<header >
+<div class="logo">
+    <a href="index.html" class="logo-link">
+        <img src="/images/logos/logo-orange.png" alt="Lusteri bend logo"></a>
+</div>
+    <nav>
+        <a href="../../../pages/svirke.html">svirke</a>
+        <a href="../../../pages/audio.html">audio</a>
+        <a href="../../../pages/video.html">video</a>
+        <a href="../../../pages/foto.html">foto</a>
+        <a href="../../../pages/bio.html">bend</a>
+        <a href="../../../pages/prodavnica.html">prodavnica</a>
+        <a href="../../../pages/kontakt.html">kontakt</a>
+    </nav>
+</header>
+`
