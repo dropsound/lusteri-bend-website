04ccf4d7aa215ebb149a72de97a1f0cfb1d04e2d
dropsound <dropsoundproduction@gmail.com>
2023-11-08 07:22:10 +0100

    Fixing gigsBase

--- src/scripting.js
+++ src/scripting.js
@@ -1,1 +1,17 @@
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+
+  function readGigs(file) { 
+    var rawFile = new XMLHttpRequest(); 
+    rawFile.open("GET", file, false); 
+    rawFile.onreadystatechange = function () { 
+      if (rawFile.readyState === 4) { 
+        if (rawFile.status === 200 || rawFile.status == 0) { 
+          var allText = rawFile.responseText; 
+          document.getElementById("gigs").innerHTML = allText; 
+        } 
+      } 
+    } 
+    rawFile.send(null); 
+  } 
+  readGigs("../gigsBase.html");
